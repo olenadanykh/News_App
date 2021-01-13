3f66a2d6aeeb27e43ceaffa118a61f5bded6d87e
Olena Danykh <olenadanykh@gmail.com>
2021-01-13 13:41:45 -0500

    setup backpack

---
+++ client/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> Hello App</h1>
+      </div>
+    );
+  }
+}
